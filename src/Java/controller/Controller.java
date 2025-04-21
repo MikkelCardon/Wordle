8bcc36baaa33edf2827a6727c9b549f696adb27a
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-04-21 11:24:44 +0200

    checks for valid input

--- src/Java/controller/Controller.java
+++ src/Java/controller/Controller.java
@@ -1,11 +1,61 @@
 package controller;
 
+import gui.cutomElement.Block;
 import javafx.event.Event;
+import storage.Storage;
 
 public class Controller {
-    private int blockID;
+    private static int blockID = 1;
+    private static String wordToGuess = "Hello";
 
     public static void keyPressed(){
+        if(checkValidInput()){
 
+        }
+
+
+        if (blockID % 5 == 0){
+            lastCellInRow();
+        }else{
+            nextCell();
+        }
+    }
+
+    private static boolean checkValidInput() {
+        String text = Storage.returnBlockByID(blockID).getText();
+
+        if (text == null || text.length() != 1) {
+            return false;
+        }
+
+        char input = text.charAt(0);
+
+        return Character.isLetter(input);
+    }
+
+    private static void lastCellInRow() {
+        int wordToGuessIndex = 0;
+
+        for (int index = blockID-4; index <= blockID; index++) {
+            Block block = Storage.returnBlockByID(index);
+            char text = block.getText().toCharArray()[0];
+
+            if (wordToGuess.charAt(wordToGuessIndex++) == text){
+                block.correctPosition();
+            }else if (wordToGuess.contains(block.getText())){
+                block.doesContainLetter();
+            }else{
+                block.doesNotContainLetter();
+            }
+        }
+
+        if (blockID >= 30){
+            nextCell();
+        }
+    }
+
+    private static void nextCell() {
+        Block block = Storage.returnBlockByID(++blockID);
+        block.requestFocus();
     }
 }
