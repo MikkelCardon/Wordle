06533a9721ac3d825ce62100ddda4db7a1149a74
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-04-21 09:58:05 +0200

    Arkitektur

---
+++ src/Java/gui/PrimaryWindow.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PrimaryWindow extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Wordle by Cardon");
+        stage.show();
+    }
+}
