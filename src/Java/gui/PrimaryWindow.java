e261c278b1220b371c3f36dac02ab9ded1e8fe61
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-04-21 14:53:53 +0200

    Styling and titel

--- src/Java/gui/PrimaryWindow.java
+++ src/Java/gui/PrimaryWindow.java
@@ -3,36 +3,53 @@ package gui;
 import controller.Controller;
 import gui.cutomElement.Block;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
 
 public class PrimaryWindow extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Wordle by Cardon");
-        GridPane gridPane = new GridPane();
-        initLayout(gridPane);
-        Scene scene = new Scene(gridPane);
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(15));
+        initLayout(root);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    private void initLayout(GridPane gridPane) {
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
+    private void initLayout(VBox root) {
+        root.setOnKeyReleased(event -> Controller.keyPressed());
 
-        gridPane.setOnKeyReleased(event -> Controller.keyPressed());
+        Label titel = new Label("WORDLE by Cardon");
+        titel.setFont(Font.font("Impact", FontWeight.BOLD, 24));
 
-        initTextFields(gridPane);
+        root.getChildren().add(titel);
+
+        initTextFields(root);
     }
 
-    private void initTextFields(GridPane gridPane){
+    private void initTextFields(VBox root){
+        GridPane boxGridPane = new GridPane();
+        boxGridPane.setVgap(5);
+        boxGridPane.setHgap(5);
+
         for (int outer = 0; outer < 6; outer++) {
             for (int inner = 0; inner < 5; inner++) {
-                gridPane.add(new Block(), inner, outer+1);
+                boxGridPane.add(new Block(), inner, outer+1);
             }
         }
+        root.getChildren().add(boxGridPane);
     }
 
 
