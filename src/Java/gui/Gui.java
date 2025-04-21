89477475abff1066c032adf0b4b745d4b0eb6dc8
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-04-21 17:13:15 +0200

    init storage

--- src/Java/gui/Gui.java
+++ src/Java/gui/Gui.java
@@ -1,9 +1,13 @@
 package gui;
 
 import javafx.application.Application;
+import storage.Storage;
 
 public class Gui {
     public static void main(String[] args) {
+        Storage.initWords();
+
         Application.launch(PrimaryWindow.class);
+
     }
 }
