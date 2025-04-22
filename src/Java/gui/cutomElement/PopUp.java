f3a7cfe11c8cdcdc4612bfcf45f63f0ae99af2cf
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-04-22 21:02:31 +0200

    Added popUp class that extends a GridPane.

---
+++ src/Java/gui/cutomElement/PopUp.java
@@ -0,0 +1,32 @@
+package gui.cutomElement;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class PopUp extends GridPane {
+    String titel;
+    String message;
+
+    public PopUp() {
+        super(100, 60);
+        styling();
+    }
+
+    private void styling() {
+        this.setAlignment(Pos.CENTER);
+
+        Label titelLabel = new Label(titel);
+        titelLabel.setStyle("-fx-font-size: 28px; -fx-font-weight: bold; -fx-text-fill: #2c3e50;");
+
+        Label messageLabel = new Label(message);
+        messageLabel.setStyle("-fx-font-size: 16px; -fx-text-fill: #555;");
+
+        Button newGameButton = new Button("New Game");
+
+        this.add(titelLabel, 0, 0);
+        this.add(messageLabel, 0, 1);
+        this.add(newGameButton, 0, 2);
+    }
+}
